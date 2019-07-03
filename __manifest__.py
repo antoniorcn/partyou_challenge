957828303ccb63193e75c9050c916f9a93e0b9ac
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-07-03 12:00:48 -0300

    [IMP] Ultimas atualizações no projeto

--- __manifest__.py
+++ __manifest__.py
@@ -23,9 +23,9 @@
 
     # always loaded
     'data': [
-        'security/security.xml',
+        'security/chatbot_security.xml',
         'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/chatbot_views.xml',
         'views/templates.xml',
         'views/partyou.xml',
         'data/status_data.xml',
